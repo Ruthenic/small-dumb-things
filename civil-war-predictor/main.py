3380ade85566596ab200a9ee6ca27681633791c3
Drake <mdrakea3@tutanota.com>
2021-01-13 20:43:53 -0500

    add civil war predictor

---
+++ civil-war-predictor/main.py
@@ -0,0 +1,6 @@
+print("Welcome to Civil War Predictor, which predicts whether your country will get in Civil War 100% Correctly.\nUnless It Doesn't.")
+country = input("What country would you like to predict Civil War for? ")
+if 'America' in country: #note that this does not include the middle east or literally any-fucking-where else, its a joke ok?
+    print('Your country will have a civil war soon!')
+else:
+    print('Your country will not have a civil war soon...')
