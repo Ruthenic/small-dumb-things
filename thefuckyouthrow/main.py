6f41774e75b2c7eb312068bac0354e56fd667a37
Drake <mdrakea3@tutanota.com>
2021-02-01 15:16:36 -0500

    add thefuckyouthrow

---
+++ thefuckyouthrow/main.py
@@ -0,0 +1,4 @@
+import time
+some_variable = "haha fuck you did you think this would be a useful variable"
+input('Hello! What is your name? ')
+raise Exception("Oh no something went wrong, oh btw here is the value of a variable: {}".format(some_variable))
