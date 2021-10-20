8a9ec37b99500c081cf3d3d4795cd50e04d5d122
Drake <53356436+Ruthenic@users.noreply.github.com>
2021-10-20 09:50:11 -0400

    add evaljs

---
+++ evaljs/index.js
@@ -0,0 +1,25 @@
+//this is meant to be minified and used in a bookmarklet to get around inspect element restrictions, along with an element picker
+(async () => {
+    window.console = { //replace the console object, because alert is basically the only way to get data out without devtools, and console doesnt normally do that
+        log: async (string) => {
+            try {
+                if (typeof(string) === 'object') { //account for the fact that objects exist. only used in log because the other two are only fallbacks for normal usage
+                    alert(JSON.stringify(string))
+                } else {
+                    alert(string);
+                }
+            } catch (e) {
+                console.error(e.toString())
+            }
+        },
+        error: async (string) => {
+            alert("ERROR: " + string);
+        },
+        warn: async (string) => {
+            alert("WARNING: " + string)
+        }
+    };
+    let code = prompt('JS to eval:');
+    let func = new Function('a', code); //the blank arg is required according to mdn
+    alert(func('a'))
+})();
