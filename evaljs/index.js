726c2550225fc0311664e45df85052088e2a00ee
Drake <53356436+Ruthenic@users.noreply.github.com>
2021-10-27 10:11:19 -0400

    evaljs: use _log in console instead of directly alerting, use console.log instead of directly alerting when evaling

--- evaljs/index.js
+++ evaljs/index.js
@@ -4,25 +4,30 @@
         log: async (string) => {
             try {
                 if (typeof(string) === 'object') { //account for the fact that objects exist. only used in log because the other two are only fallbacks for normal usage
-                    alert(JSON.stringify(string))
+                    console._log(JSON.stringify(string))
                 } else {
-                    alert(string);
+                    console._log(string);
                 }
             } catch (e) {
                 console.error(e.toString())
             }
         },
         error: async (string) => {
-            alert("ERROR: " + string);
+            console._log("ERROR: " + string);
         },
         warn: async (string) => {
-            alert("WARNING: " + string)
+            console._log("WARNING: " + string)
+        },
+        _log: async (string) => {
+            alert(string)
         }
-    };
+    }; //TODO: use a proxy
     let code = prompt('JS to eval:');
     let func = new Function('a', code); //the blank arg is required according to mdn
     try {
         let res = func('a')
+        console.log(res)
+        return res //i don't wanna know why you'd wanna use this value but return it anyways
     } catch(e) {
         console.error(e.toString())
     } 
